9aed0112ea8fe0faf06a944098d126813b822f8f
KarnSakulngam <karnsakulngam@gmail.com>
2023-07-13 17:08:14 +0700

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,57 @@
+# if True:
+#     print("Hello")
+
+# if False:
+#     print("Hello1")
+
+# money = 100
+
+# if money >= 300:
+#     print("KFC!!!")
+
+# else:
+#     print("byebye")
+
+username = "KarnSakulngam"
+
+password = "123ab"
+
+userLogin = input("Enter your username: ")
+
+passLogin = input("Enter your password: ")
+
+# if userLogin == username and passLogin == password:
+#     print("Success")
+#     print("1 = Vat Cal")
+#     print("2 = cal")
+#     num = input("Select Number>>")
+#     if num == "1":
+#         price = int(input("Enter Your Price"))
+#         vat = 7/100
+#         print(price + (price * vat))
+#     elif num == "2":
+#         x = int(input("enter number"))
+#         y = int(input("enter number"))
+#         print(x + y)
+
+# elif userLogin == username:
+#     print("only username")
+
+# elif userLogin == password:
+#     print("only password")
+# else:
+#     print("Incorrect")
+
+# x = 0
+
+# while userLogin != username and passLogin != password and x == 0:
+#     userLogin = input("Enter your username: ")
+#     passLogin = input("Enter your password: ")
+
+#     x += 1
+
+# if userLogin == username and passLogin == password:
+#     print("Done")
+
+# while True:
+#     print("hello")
